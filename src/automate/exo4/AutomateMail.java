cdf57257549c1d8b33b0930fa6c5355dfb5317cc
Ptit-Biscuit <vincent.brebion@etu.univ-nantes.fr>
2016-11-29 08:45:37 +0100

    commit

--- src/automate/exo4/AutomateMail.java
+++ src/automate/exo4/AutomateMail.java
@@ -13,12 +13,33 @@ import java.util.HashMap;
 public class AutomateMail extends Automate implements Validable {
 
     public AutomateMail() {
-        this.addEtatsInitAndFinal(new Automate.Etat("E0", true, false),
-                new Automate.Etat("E5", false, true));
+        Etat[] etats = new Etat[] {
+                new Automate.Etat("E0", true, false),
+                new Automate.Etat("E1"),
+                new Automate.Etat("E2"),
+                new Automate.Etat("E3"),
+                new Automate.Etat("E4"),
+                new Automate.Etat("E5", false, true),
+        };
 
-        char[] chars = new char[] {};
+        this.addEtatsInitAndFinal(etats[0], etats[5]);
 
-        System.out.println(Arrays.toString(chars));
+        Character[] chars = new Character[] {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+                'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y' ,'z',
+                'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+                'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+                '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
+        };
+
+        this.addSameTransition(this.getEtatInit(), etats[1], chars);
+        this.addSameTransition(etats[1], etats[1], chars);
+        this.addTransition(etats[1], etats[2], '.');
+        this.addSameTransition(etats[2], etats[3], chars);
+        this.addSameTransition(etats[3], etats[3], chars);
+        this.addTransition(etats[3], etats[4], '@');
+        this.addSameTransition(etats[4], this.getEtatFinal(), chars);
+        this.addSameTransition(this.getEtatFinal(), this.getEtatFinal(), chars);
+        this.addTransition(this.getEtatFinal(), this.getEtatInit(), ';');
     }
 
     @Override
